1a419cece45f08d64dfa73ff8787b444c3a4ba22
dj2548 <113076714+dj2548@users.noreply.github.com>
2022-10-04 10:50:21 -0400

    Update pig latin 2.cs

--- pig latin 2/pig latin 2.cs
+++ pig latin 2/pig latin 2.cs
@@ -1,97 +1,44 @@
-﻿namespace PigLatin
+using System.Diagnostics.Metrics;
+using System.Diagnostics.Tracing;
+
+namespace piggy_latin
 {
     internal class Program
     {
-
-        //As we learn more and more, the job of main shrinks to delegating calls to methods and our continue loop
         static void Main(string[] args)
         {
-            bool keepGoing = true;
+            string word = Console.ReadLine().ToLower();
 
-            while (keepGoing == true)
+            while (word == string.Empty)
             {
-                string input = GetUserInput("Hello, please input a word to translate into Pig Latin.");
-                string pigLatinWord = Translate(input);
-                Console.WriteLine(pigLatinWord);
-
-
+                Console.WriteLine("Enter a word or phrase please.");
+                word = Console.ReadLine();
             }
-            //Console.WriteLine(FindVowelPosition("hello"));
-            //Console.WriteLine(FindVowelPosition("Sky"));
-            //Console.WriteLine(FindVowelPosition("Strong"));
-            //Console.WriteLine(FindVowelPosition("Peach"));
-            //Console.WriteLine(FindVowelPosition("Apple"));
 
-
-        }
-
-        //ContainsVowe()l 
-        //IsVowel(char c) 
-
-        //FindVowelPosition() 
-
-        //Message is what prompts the user
-        public static string GetUserInput(string prompt)
-        {
-            Console.WriteLine(prompt);
-            string input = Console.ReadLine().ToLower();
-
-            return input;
-        }
-
-
-        public static bool IsVowel(char c)
-        {
-            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
-
-            bool isVowel = vowels.Contains(c);
-
-            return isVowel;
-        }
-
-        public static int FindVowelPosition(string word)
-        {
-            for (int i = 0; i < word.Length; i++)
+            string[] words = word.Split(' ');
+            //string[] currWord = words[0].Split(' ');
+            string vowelWordEnd = "way";
+            string conWordEnd = "ay";
+            char letter;
+            int vowelPosition = -1;
+            string newSentence = "";
+            foreach (string currWord in words) 
             {
-                char c = word[i];
-                bool isVowel = IsVowel(c);
-                if (isVowel == true)
+                string currWordLower = currWord.ToLower();
+                if ((currWordLower[0] == 'a' || currWordLower[0] == 'e' || currWordLower[0] == 'i' || currWordLower[0] == 'o' || currWordLower[0] == 'u')) 
+                 {
+                    newSentence += currWord + vowelWordEnd + " ";
+                } else 
                 {
-                    return i;
+                    newSentence += currWord + " ";
                 }
-            }
+                   
 
-            //Say we get through the whole word and no vowels are found 
-            return -1;
-        }
-
-        public static string Translate(string word)
-        {
-            int pos = FindVowelPosition(word);
-            string result = "";
-            //This means our word starts with a vowel
-            if (pos == 0)
-            {
-                result = word + "way";
+               
             }
-            else if (pos == -1)
-            {
-                Console.WriteLine("No vowel was found, skipping translating this word");
-                result = word;
-            }
-            else if (pos > 0)
-            {
-                //We know the position of the first vowel, we want to cut off everything before it 
-                //and move it to the end. 
-                //strong 
-                string prefix = word.Substring(pos);
-                string postFix = word.Substring(0, pos);
-                result = prefix + postFix + "ay";
-            }
-
-            //Console.WriteLine(result);
-
-            return result;
+            Console.WriteLine(newSentence);
         }
     }
 }
+
+
